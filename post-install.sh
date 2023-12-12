2a0b2bb04b1839822ca291f45f623ac0e7f50fe8
Janik <ledonk@fedora.fritz.box>
2023-12-12 18:37:17 +0100

    added PATH var

--- post-install.sh
+++ post-install.sh
@@ -1,14 +1,18 @@
+#! /usr/bin/bash
+
+export PATH=~/git/repo/
+
 #create directories
 mkdir -p git/repo/{dotfiles,minecraft,images}
 
 
 #clone git repositories
-git clone git@github.com:led0nk/dotfiles.git ~/git/repo/dotfiles
-git clone git@github.com:led0nk/minecraft.git ~/git/repo/minecraft
-git clone git@github.com:led0nk/images.git ~/git/repo/images
+git clone git@github.com:led0nk/dotfiles.git $PATH/dotfiles
+git clone git@github.com:led0nk/minecraft.git $PATH/minecraft
+git clone git@github.com:led0nk/images.git $PATH/repo/images
 
 
 #create systemlinks for dotfiles
-ln -s ~/git/repo/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/git/repo/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
-ln -s ~/git/repo/dotfiles/gitconfig/.gitconfig ~/.gitconfig
+ln -s $PATH/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s $PATH/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s $PATH/dotfiles/gitconfig/.gitconfig ~/.gitconfig
