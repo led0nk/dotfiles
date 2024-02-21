8f2ec1c9afdbd45af0b6d14f0fc91eb759b7aa16
Janik <10290002+led0nk@users.noreply.github.com>
2024-02-21 01:48:38 +0100

    added .sh lsp to nvim

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -8,6 +8,7 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.shfmt,
 				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.diagnostics.eslint,
 				null_ls.builtins.diagnostics.html,
