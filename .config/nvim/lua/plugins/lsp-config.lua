80321d0e9711c13522ffbf54cd7db80fa3da9ebf
Janik <10290002+led0nk@users.noreply.github.com>
2024-03-29 19:38:29 +0100

    nvim keybind

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -56,7 +56,7 @@ return {
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
 			vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_previous)
+			vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev)
 		end,
 	},
 }
