6f996201da40ebab05889aea49b850b4792d2ca6
Janik <10290002+led0nk@users.noreply.github.com>
2024-04-08 01:05:01 +0200

    nvim/none-ls/prettier-fileformat

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,12 +6,13 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				--	null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = { "html", "yaml", "css", "json" },
+				}),
 				null_ls.builtins.formatting.goimports,
-				--null_ls.builtins.formatting.yamlfmt,
 				--null_ls.builtins.formatting.shfmt,
 				null_ls.builtins.diagnostics.golangci_lint,
-				null_ls.builtins.diagnostics.html,
+				null_ls.builtins.diagnostics.htmllint,
 				null_ls.builtins.diagnostics.checkmake,
 				null_ls.builtins.diagnostics.yamllint,
 			},
