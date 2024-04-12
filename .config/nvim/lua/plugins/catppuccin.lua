8390394c02b07b906b3f017f3ed46d1ed8fb550a
Janik <10290002+led0nk@users.noreply.github.com>
2024-04-12 19:52:22 +0200

    nvim/telescope

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,18 @@
-return { "catppuccin/nvim",
-          lazy = false,
-          name = "catppuccin",
-          priority = 1000,
-          config = function()
-            vim.cmd.colorscheme "catppuccin"
-          end
+--function ColorMyPencils(color)
+--	color = color or "catppuccin"
+--	vim.cmd.colorscheme(color)
+--
+--	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--end
+
+return {
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin")
+		--		ColorMyPencils()
+	end,
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown(),
+						require("telescope.themes").get_dropdown,
 					},
 				},
 				pickers = {
@@ -20,13 +20,18 @@ return {
 				},
 			})
 			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("notify")
 			local builtin = require("telescope.builtin")
+			local themes = require("telescope.themes")
+			local opts = { hidden = true }
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>ht", builtin.colorscheme, {})
 			vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
 			vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+			vim.keymap.set("n", "<leader>fn", ":Telescope notify<CR>", {})
 		end,
 	},
 	{
