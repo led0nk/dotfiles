a2e8a3341d55e58306d5b09f73f563039a8d7d3d
Janik <10290002+led0nk@users.noreply.github.com>
2024-04-15 01:14:42 +0200

    nvim/rose-pine + telescope

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,18 +1,43 @@
 --function ColorMyPencils(color)
---	color = color or "catppuccin"
---	vim.cmd.colorscheme(color)
+--  color = color or "catppuccin"
+--  vim.cmd.colorscheme(color)
 --
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 --end
 
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-		--		ColorMyPencils()
-	end,
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				-- transparent_background = true,
+				flavour = "macchiato",
+			})
+			--vim.cmd.colorscheme("catppuccin")
+			--ColorMyPencils()
+		end,
+	},
+	{
+		"rose-pine/nvim",
+		lazy = false,
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				variant = "main",
+				dark_variant = "main",
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = false,
+				},
+			})
+			vim.cmd.colorscheme("rose-pine")
+			--ColorMyPencils()
+		end,
+	},
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,34 +1,34 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier.with({
-          filetypes = { "html", "yaml", "css" },
-        }),
-        null_ls.builtins.formatting.goimports,
-        --null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.diagnostics.htmllint,
-        null_ls.builtins.diagnostics.checkmake,
-        null_ls.builtins.diagnostics.yamllint,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = { "html", "yaml", "css" },
+				}),
+				null_ls.builtins.formatting.goimports,
+				--null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.diagnostics.htmlhint,
+				null_ls.builtins.diagnostics.checkmake,
+				null_ls.builtins.diagnostics.yamllint,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
