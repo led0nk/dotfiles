428a0a538a5ac209ba75bd21085f50f33a8c2d67
Janik <10290002+led0nk@users.noreply.github.com>
2024-05-13 09:40:25 +0200

    notify theme

--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -26,22 +26,22 @@ return {
 			end
 
 			-- Update colors to use catpuccino colors
-			vim.cmd([[
-        highlight NotifyERRORBorder guifg=#ed8796
-        highlight NotifyERRORIcon guifg=#ed8796
-        highlight NotifyERRORTitle  guifg=#ed8796
-        highlight NotifyINFOBorder guifg=#8aadf4
-        highlight NotifyINFOIcon guifg=#8aadf4
-        highlight NotifyINFOTitle guifg=#8aadf4
-        highlight NotifyWARNBorder guifg=#f5a97f
-        highlight NotifyWARNIcon guifg=#f5a97f
-        highlight NotifyWARNTitle guifg=#f5a97f
-        highlight link NotifyERRORBody Normal
-        highlight link NotifyWARNBody Normal
-        highlight link NotifyINFOBody Normal
-        highlight link NotifyDEBUGBody Normal
-        highlight link NotifyTRACEBody Normal
-      ]])
+			--	vim.cmd([[
+			--    highlight NotifyERRORBorder guifg=#ed8796
+			--    highlight NotifyERRORIcon guifg=#ed8796
+			--    highlight NotifyERRORTitle  guifg=#ed8796
+			--    highlight NotifyINFOBorder guifg=#8aadf4
+			--    highlight NotifyINFOIcon guifg=#8aadf4
+			--    highlight NotifyINFOTitle guifg=#8aadf4
+			--    highlight NotifyWARNBorder guifg=#f5a97f
+			--    highlight NotifyWARNIcon guifg=#f5a97f
+			--    highlight NotifyWARNTitle guifg=#f5a97f
+			--    highlight link NotifyERRORBody Normal
+			--    highlight link NotifyWARNBody Normal
+			--    highlight link NotifyINFOBody Normal
+			--    highlight link NotifyDEBUGBody Normal
+			--    highlight link NotifyTRACEBody Normal
+			--  ]])
 		end,
 	},
 }
