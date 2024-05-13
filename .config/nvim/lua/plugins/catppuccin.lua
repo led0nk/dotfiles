a86002c1360747dfd6278c8299e5e2ab469f93eb
Janik <10290002+led0nk@users.noreply.github.com>
2024-05-13 09:37:49 +0200

    onedark

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -43,7 +43,43 @@ return {
 					--background = "#0f1119",
 				},
 			})
-			vim.cmd.colorscheme("rose-pine")
+			--vim.cmd.colorscheme("rose-pine")
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		lazy = false,
+		name = "onedark",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({
+				style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+				transparent = false, -- Show/hide background
+				term_colors = true, -- Change terminal color as per the selected theme style
+				ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+				cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+				toggle_style_key = nil,
+				toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+				code_style = {
+					comments = "italic",
+					keywords = "none",
+					functions = "none",
+					strings = "none",
+					variables = "none",
+				},
+				lualine = {
+					transparent = false, -- lualine center bar transparency
+				},
+				colors = {}, -- Override default colors
+				highlights = {}, -- Override highlight groups
+				diagnostics = {
+					darker = true, -- darker colors for diagnostic
+					undercurl = true, -- use undercurl instead of underline for diagnostics
+					background = true, -- use background color for virtual text
+				},
+			})
+			require("onedark").load()
+			vim.cmd.colorscheme("onedark")
 		end,
 	},
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,22 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "rose-pine",
-      },
-      sections = {
-        lualine_x = {
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = { fg = "#ff9e64" },
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "onedark",
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
