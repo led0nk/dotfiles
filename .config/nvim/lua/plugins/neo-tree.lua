263f33f1abc7e5841574b67dabc7f4cd50fcac33
Janik <10290002+led0nk@users.noreply.github.com>
2024-05-13 08:37:51 +0200

    fixed neo-tree, added vim-fugitive

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,21 +1,27 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-    },
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v2.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+
+	config = function()
+		require("neo-tree").setup({
+
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						".git",
+					},
+				},
+			},
+			never_show = {},
+			vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>"),
+		})
+	end,
 }
