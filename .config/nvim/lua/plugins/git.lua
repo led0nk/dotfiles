0c4207e55e1983c24d5ccd10dfa2f762e69787a9
Janik <10290002+led0nk@users.noreply.github.com>
2024-05-13 08:38:51 +0200

    git

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",           -- optional
+    },
+    config = function()
+      local neogit = require("neogit")
+      neogit.setup({
+        config = true,
+      })
+      vim.keymap.set("n", "<leader>ng", ":Neogit<CR>")
+    end,
+  },
+}
