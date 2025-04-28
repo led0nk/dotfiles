4d2beea8e33738a7b136f98d4ff7eba2e1d1167c
Janik Knodel <10290002+led0nk@users.noreply.github.com>
2025-04-28 09:01:06 +0200

    rebase §

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,107 +1,108 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      require("telescope").setup({
-        pickers = {
-          find_files = {
-            hidden = true,
-            no_ignore = true,
-            follow = true,
-          },
-        },
-        defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "-L",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--hidden",
-          },
-          prompt_prefix = "   ",
-          selection_caret = "  ",
-          entry_prefix = "  ",
-          initial_mode = "insert",
-          selection_strategy = "reset",
-          sorting_strategy = "descending",
-          layout_strategy = "horizontal",
-          layout_config = {
-            horizontal = {
-              prompt_position = "bottom",
-              preview_width = 0.55,
-              results_width = 0.8,
-            },
-            vertical = {
-              mirror = false,
-            },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 120,
-          },
-          file_sorter = require("telescope.sorters").get_fuzzy_file,
-          file_ignore_patterns = { "node_modules", "^%.git/", "vendor" },
-          generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-          path_display = { "truncate" },
-          winblend = 0,
-          border = {},
-          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          color_devicons = true,
-          set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-          file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-          grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-          qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-        },
-      })
-      require("telescope").load_extension("notify")
-      -- require("telescope").load_extension("remote-sshfs")
-      local builtin = require("telescope.builtin")
-      local themes = require("telescope.themes")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-      vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Git Files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-      vim.keymap.set("n", "<leader>fs", function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end, { desc = "Grep String" })
-      vim.keymap.set("n", "<leader>fw", function()
-        local word = vim.fn.expand("<cword>")
-        builtin.grep_string({ search = word })
-      end, { desc = "Find word" })
-      vim.keymap.set("n", "<leader>fW", function()
-        local word = vim.fn.expand("<cWORD>")
-        builtin.grep_string({ search = word })
-      end, { desc = "Find WORD" })
-      vim.keymap.set("n", "<leader>fb", function()
-        builtin.buffers(themes.get_ivy())
-      end, { desc = "Find Buffers" })
-      vim.keymap.set("n", "<leader>ht", builtin.colorscheme, { desc = "Telescope Colorscheme" })
-      -- vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "LSP References" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" })
-      vim.keymap.set(
-        "n",
-        "<leader>ws",
-        builtin.lsp_dynamic_workspace_symbols,
-        { desc = "LSP Dynamic Workspace Symbols" }
-      )
-      vim.keymap.set("n", "<leader>ft", builtin.lsp_type_definitions, { desc = "LSP type definitions" })
-      vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations, { desc = "LSP implementations" })
-      vim.keymap.set("n", "<leader>gb", function()
-        builtin.git_branches({ layout_config = { preview_width = 0.6 } })
-      end, { desc = "Git branches" })
-      vim.keymap.set("n", "<leader>gc", function()
-        builtin.git_commits({ layout_config = { preview_width = 0.6 } })
-      end, { desc = "Git commits" })
-      --			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
-      vim.keymap.set("n", "<leader>fn", ":Telescope notify<CR>", { desc = "Telescope notify" })
-      vim.keymap.set("n", "<leader>fo", ":ObsidianQuickSwitch<CR>", { desc = "Telescope Obsidian" })
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+						no_ignore = true,
+						follow = true,
+					},
+				},
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"-L",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+					},
+					prompt_prefix = "   ",
+					selection_caret = "  ",
+					entry_prefix = "  ",
+					initial_mode = "insert",
+					selection_strategy = "reset",
+					sorting_strategy = "descending",
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							prompt_position = "bottom",
+							preview_width = 0.55,
+							results_width = 0.8,
+						},
+						vertical = {
+							mirror = false,
+						},
+						width = 0.87,
+						height = 0.80,
+						preview_cutoff = 120,
+					},
+					file_sorter = require("telescope.sorters").get_fuzzy_file,
+					file_ignore_patterns = { "node_modules", "^%.git/", "vendor" },
+					generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+					path_display = { "truncate" },
+					winblend = 0,
+					border = {},
+					borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					color_devicons = true,
+					set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+					file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+					grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+					qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+				},
+			})
+			require("telescope").load_extension("notify")
+			-- require("telescope").load_extension("remote-sshfs")
+			local builtin = require("telescope.builtin")
+			local themes = require("telescope.themes")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+			vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Git Files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+			vim.keymap.set("n", "<leader>fs", function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end, { desc = "Grep String" })
+			vim.keymap.set("n", "<leader>fw", function()
+				local word = vim.fn.expand("<cword>")
+				builtin.grep_string({ search = word })
+			end, { desc = "Find word" })
+			vim.keymap.set("n", "<leader>fW", function()
+				local word = vim.fn.expand("<cWORD>")
+				builtin.grep_string({ search = word })
+			end, { desc = "Find WORD" })
+			vim.keymap.set("n", "<leader>fb", function()
+				builtin.buffers(themes.get_ivy())
+			end, { desc = "Find Buffers" })
+			vim.keymap.set("n", "<leader>ht", builtin.colorscheme, { desc = "Telescope Colorscheme" })
+			-- vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "LSP References" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" })
+			vim.keymap.set(
+				"n",
+				"<leader>ws",
+				builtin.lsp_dynamic_workspace_symbols,
+				{ desc = "LSP Dynamic Workspace Symbols" }
+			)
+			vim.keymap.set("n", "<leader>ft", builtin.lsp_type_definitions, { desc = "LSP type definitions" })
+			vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations, { desc = "LSP implementations" })
+			vim.keymap.set("n", "<leader>gb", function()
+				builtin.git_branches({ layout_config = { preview_width = 0.6 } })
+			end, { desc = "Git branches" })
+			vim.keymap.set("n", "<leader>gc", function()
+				builtin.git_commits({ layout_config = { preview_width = 0.6 } })
+			end, { desc = "Git commits" })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
+			vim.keymap.set("n", "<leader>fn", ":Telescope notify<CR>", { desc = "Telescope notify" })
+			vim.keymap.set("n", "<leader>fo", ":ObsidianQuickSwitch<CR>", { desc = "Telescope Obsidian" })
+			vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>", { desc = "Telescope projects" })
+		end,
+	},
 }
